55a66bd4b32ec3505b406b18a9b9150d37f063a8
martinrysanek <33803015+martinrysanek@users.noreply.github.com>
2023-09-04 00:16:42 +0200

    v2
    New

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # set up root route
 @app.route("/")
 def hello_world():
-	return "Hello World"
+	return "Hello World New"
 
 # Get the PORT from environment
 port = os.getenv('PORT', '8080')
