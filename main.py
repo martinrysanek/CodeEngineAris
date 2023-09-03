eba3f92b7c8969caf7996cc38660ca7784827dc9
martinrysanek <33803015+martinrysanek@users.noreply.github.com>
2023-09-04 00:46:37 +0200

    v3
    ICO

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from flask import Flask
+import xml.etree.ElementTree as ET
+import urllib.request
 import os
 
 app = Flask(__name__)
 
+# Function to traverse the entire XML tree and display attributes of a given tag name
+def find_and_display_attributes(element, tag_name):
+    if element.tag.endswith(tag_name):
+        text = element.text
+        if text:
+            # print(f"{tag_name} text: {text}")                
+            return text
+        else:
+            return "Unknown"
+    for child in element:
+        name = find_and_display_attributes(child, tag_name)  
+        if name != "Unknown":
+            return name
+    return "Unknown"
+
 # set up root route
 @app.route("/")
-def hello_world():
-	return "Hello World New"
+def aris():
+  url = 'https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=14890992'
+  response = urllib.request.urlopen(url)
+  data = response.read()
+  # Parse the XML string
+  root = ET.fromstring(data)
+  # Example: Display all attributes for the 'person' tag throughout the XML tree
+  name = find_and_display_attributes(root, 'OF')
+  return name
 
 # Get the PORT from environment
 port = os.getenv('PORT', '8080')
