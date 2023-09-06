8006592c7c03e2ac8ec91918f4391c6d5221f69d
martinrysanek <33803015+martinrysanek@users.noreply.github.com>
2023-09-06 08:04:33 +0200

    v10

--- main.py
+++ main.py
@@ -79,10 +79,10 @@ logger.addHandler(string_handler)
 
 # Log some messages
 logger.debug("This is a debug message")
-logger.info("This is an info message")
-logger.warning("This is a warning message")
-logger.error("This is an error message")
-logger.critical("This is a critical message")
+# logger.info("This is an info message")
+# logger.warning("This is a warning message")
+# logger.error("This is an error message")
+# logger.critical("This is a critical message")
 
 # Get the PORT from environment
 port = os.getenv('PORT', '8080')
